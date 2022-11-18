d213599ace9ce8e54817016cbcce493855a62ad4
Angelo Andrade <angelojrrj@gmail.com>
2022-11-18 19:05:00 -0300

    responsive background

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Menu} from './components/menu'
+import bodyShape from './images/body-shape.svg'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Menu />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <div id='backgroundDiv'><div id='backgroundImage'></div></div>
+      <Menu />
+    </React.StrictMode>
+  </>
 );
