31c0b4a8193809f56dcccd3faed8eaa54152e606
Angelo Andrade <angelojrrj@gmail.com>
2022-11-18 20:32:38 -0300

    assinarNewsletter added

--- src/components/assinarNewsletter/index.js
+++ src/components/assinarNewsletter/index.js
@@ -1,9 +1,12 @@
-
+import styles from './index.module.css'
 
 export function AssinarNewsletter () {
     return (
-        <>
-            
-        </>
+        <div className={styles.divNewsletter}>
+            <h2>Sua casa com as </h2>
+            <h1 className={styles.h1}>melhores plantas</h1>
+            <p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. 
+                Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+        </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Menu} from './components/menu'
-import bodyShape from './images/body-shape.svg'
+import { AssinarNewsletter } from './components/assinarNewsletter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <React.StrictMode>
-      <div id='backgroundDiv'><div id='backgroundImage'></div></div>
       <Menu />
+      <AssinarNewsletter />
     </React.StrictMode>
   </>
 );
