4fed86b76842e6788a31ba5da7c0fdc7284d7d03
Angelo Andrade <angelojrrj@gmail.com>
2022-11-21 08:12:51 -0300

    styled-components main index, done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {Menu} from './components/menu'
 import { AssinarNewsletter } from './components/assinarNewsletter';
-import {GlobalStyle} from './styled.jsx'
+import {Background, GlobalStyle} from './styled.jsx'
 
+import BackgroundBg from './images/bg.jpg';
+import BackgroundLines from './images/Linhas.svg';
+import BodyShape from './images/body-shape.svg';
+
+const bgQuery ={'@media screen and (maxWidth: 900px)' :{visibility: 'hidden'}};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+
     <React.StrictMode>
       <GlobalStyle />
-      <div id='bg1'></div>
-      <div id='bg2'></div>
-      <div id='bg3'></div>
+      <Background repeat={'repeat'} position={'center'} url={BackgroundBg}  />
+      <Background repeat={'repeat'} position={'center'} url={BackgroundLines} />
+      <Background repeat={'no-repeat'} position={'right top'} url={BodyShape} style={bgQuery} visibility={'hidden'}/>
       <Menu />
       <AssinarNewsletter />
     </React.StrictMode>
--- src/styled.jsx
+++ src/styled.jsx
@@ -2,18 +2,38 @@ import styled, {createGlobalStyle} from "styled-components";
 
 
 export const GlobalStyle = createGlobalStyle `
+    @charset "UTF-8";
+    body {
+        margin: 0;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        
+    }
 
-body {
-    margin: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    
-  }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-  * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
+    .bgQuery {
+        @media screen and (max-width: 900px) {}
+    }
 ` 
+
+export const Background = styled.div `
+
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -999;
+
+    background-repeat: ${(props) => props.repeat} ;
+    background-position: ${(props) => props.position};
+    background-image: url('${(props) => props.url}');
+
+    @media screen and (max-width: 900px) {
+        visibility: ${(props) => props.visibility};
+        }
+
+`
